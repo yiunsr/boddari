6961ee938391d21e7b7520eb6798b56aacb51216
yiunsr <yiunsr@hanmail.net>
2023-03-12 17:11:36 +0900

    wikinews 컬럼명 수정

--- wikinews/dump.py
+++ wikinews/dump.py
@@ -11,7 +11,7 @@ CSV_FILE = os.path.join(DIR_PATH, PROJECT_NAME + ".csv")
 def get_infos():
     conn = sqlite3.connect(SQLITE_FILE)
     cur = conn.cursor()
-    sql = """select DISTINCT  title, body, content_date from scrap s
+    sql = """select DISTINCT  title, body, content_date, url from scrap s
         where s.title is not null and s.title != '' and length(s.body) >30
     order by content_date asc, id asc
     """
@@ -23,14 +23,16 @@ def get_infos():
         item["제목"] = row[0]
         item["내용"] = row[1]
         item["작성일"] = row[2]
+        item["출처"] = row[3]
         infos.append(item)
     return infos
 
 def write_csv(infos):
     csv_file = open(CSV_FILE,'w', newline='', encoding="UTF-8")
     csv_write = csv.writer(csv_file)
-    fieldnames = ['작성일', '제목', '내용']
+    fieldnames = ['작성일', '출처', '제목', '내용']
     csv_write = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_write.writeheader()
 
     for info in infos:
         csv_write.writerow(info)
