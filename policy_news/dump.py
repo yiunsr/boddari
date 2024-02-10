99fe5f289a00bf3812cafc1331f18b2b7e52c469
yiunsr <yiunsr@hanmail.net>
2024-02-10 23:14:09 +0900

    csv 파일과 txt 파일 분리 저장

--- policy_news/dump.py
+++ policy_news/dump.py
@@ -3,6 +3,7 @@ import glob
 import os
 import io
 import zipfile
+from pathlib import Path
 from multiprocessing import Pool
 
 NUM_PROCESS = 6
@@ -25,8 +26,8 @@ def read_file(filepath):
             license_type =  row["공공저작물 유형"]
             if license_type != "유형1":
                 continue
-            title = row["정책정보 메인 제목"]
-            body = row["정책정보 본문 내용"]
+            title = row["정책정보 메인 제목"].strip()
+            body = row["정책정보 본문 내용"].strip()
             if len(body) < 100:
                 continue
             item = {"id":id_, "section": section, "작성일": date_str,
@@ -58,7 +59,7 @@ def write_csv(total_infos, file_len):
             start = end
         file_idx_str = str(file_idx + 1).zfill(2)
         zip_file_path = os.path.join(
-            DIR_PATH, PROJECT_NAME + "." + file_idx_str + ".zip")
+            DIR_PATH, "csv", PROJECT_NAME + "." + file_idx_str + ".csv.zip")
         with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_BZIP2) as zip_file:
             io_csv = io.StringIO()
             fieldnames = ['id', '작성일', 'section', '제목', '내용']
@@ -69,10 +70,41 @@ def write_csv(total_infos, file_len):
             zip_file.writestr(PROJECT_NAME + "." + file_idx_str + ".csv",
                               io_csv.getvalue())
 
+def write_txt(total_infos, file_len):
+    info_len = len(total_infos)
+    split_size = int(info_len / file_len)
+    start = 0
+    for file_idx in range(file_len):
+        if file_idx + 1 == file_len:
+            infos = total_infos[start:]
+        else:
+            end = split_size * (file_idx + 1)
+            infos = total_infos[start:end]
+            start = end
+        file_idx_str = str(file_idx + 1).zfill(2)
+        zip_file_path = os.path.join(
+            DIR_PATH, "txt", PROJECT_NAME + "." + file_idx_str + ".txt.zip")
+        with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_BZIP2) as zip_file:
+            io_file = io.StringIO()
+            for info in infos:
+                io_file.write(info['제목'])
+                io_file.write("\n")
+                io_file.write(info['내용'])
+                io_file.write("\n\n")
+            zip_file.writestr(PROJECT_NAME + "." + file_idx_str + ".txt",
+                              io_file.getvalue())
+
 
 def main():
+    csv_folder = os.path.join(DIR_PATH, "csv")
+    Path(csv_folder).mkdir(parents=True, exist_ok=True)
+
+    txt_folder = os.path.join(DIR_PATH, "txt")
+    Path(txt_folder).mkdir(parents=True, exist_ok=True)
+
     infos = read_files()
     write_csv(infos, 4)
+    write_txt(infos, 4)
 
 if __name__ == "__main__":
     print("======== dump start ========")
